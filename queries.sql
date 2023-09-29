d6d481b41a6bd403f76dfe028f5a1f58637b65c0
Mike Dark <agyemankojo47@gmail.com>
2023-09-29 12:10:56 +0000

    Create queries

---
+++ queries.sql
@@ -0,0 +1,59 @@
+BEGIN;
+-- Start a Transaction:
+BEGIN
+
+-- Update "species" to "unspecified" for Animals with Names Ending in "mon":
+UPDATE
+  animals
+SET
+  species = 'unspecified'
+WHERE
+  name LIKE '%mon';
+
+UPDATE
+  6
+
+  -- Update "species" to "pokemon" for Animals without a Species:
+UPDATE
+  animals
+SET
+  species = 'pokemon'
+WHERE
+  species IS NULL;
+
+UPDATE
+  5 -- TAKE A BREATH 
+  BEGIN;
+
+BEGIN
+
+-- Delete Animals Born After Jan 1st, 2022:
+DELETE FROM
+  animals
+WHERE
+  date_of_birth > '2022-01-01';
+
+DELETE 1 SAVEPOINT before_weight_update;
+
+-- Create a Savepoint for the Transaction:
+SAVEPOINT
+UPDATE
+  animals
+SET
+  weight_kg = - weight_kg;
+
+UPDATE
+  10 ROLLBACK TO SAVEPOINT before_weight_update;
+
+ROLLBACK
+
+-- Update Animals' Weights That Are Negative:
+UPDATE
+  animals
+SET
+  weight_kg = - weight_kg
+WHERE
+  weight_kg < 0;
+
+UPDATE
+  4
